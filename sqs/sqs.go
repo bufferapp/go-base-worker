aa5ff5aeac4eb97fb461c7d555abb9f14eddc692
Eric Khun <eric@buffer.com>
2017-09-28 05:30:55 +0000

    Receive Message 1 by 1

--- sqs/sqs.go
+++ sqs/sqs.go
@@ -1,6 +1,9 @@
 package sqs
 
 import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
@@ -25,4 +28,31 @@ func Initialize(url string) Type {
 		service:  svc,
 	}
 	return s
+}
+
+// Receive SQS message
+func Receive(sqsSvc Type) (*sqs.Message, error) {
+
+	result, err := sqsSvc.service.ReceiveMessage(&sqs.ReceiveMessageInput{
+		QueueUrl:            aws.String(sqsSvc.queueURL),
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout:   aws.Int64(36000),
+		WaitTimeSeconds:     aws.Int64(0),
+	})
+
+	if err != nil {
+		fmt.Println("Something went wrong...", err)
+		return nil, err
+	}
+
+	if len(result.Messages) == 0 {
+		fmt.Println("Received no messages")
+		return nil, nil
+	}
+
+	for _, item := range result.Messages {
+		return item, nil
+	}
+
+	return nil, nil
 }
