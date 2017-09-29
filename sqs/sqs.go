9267b18e9433fa4ca0d70ad5ec726e66534ef44f
Eric Khun <eric@buffer.com>
2017-09-29 03:09:05 +0000

    More idiomatic sqs package

--- sqs/sqs.go
+++ sqs/sqs.go
@@ -1,75 +1,56 @@
 package sqs
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/pkg/errors"
 )
 
-// SQSType all things SQS
-type SQSType struct {
-	QueueURL string
-	Service  *sqs.SQS
+// Client all things SQS
+type Client struct {
+	queueURL string
+	client   *sqs.SQS
 }
 
-// Initialize the SQS queue service
-func Initialize(url string) SQSType {
-
-	// TODO: This read ~/.aws/config, Better to explicit the env variable
+// NewClient creates a SQS client.
+func NewClient(queueURL string) *Client {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 
-	svc := sqs.New(sess)
-	s := SQSType{
-		QueueURL: url,
-		Service:  svc,
+	return &Client{
+		queueURL: queueURL,
+		client:   sqs.New(sess),
 	}
-	return s
 }
 
-// Receive SQS message
-func Receive(sqsSvc SQSType) (*sqs.Message, error) {
-
-	result, err := sqsSvc.Service.ReceiveMessage(&sqs.ReceiveMessageInput{
-		QueueUrl:            aws.String(sqsSvc.QueueURL),
+// Receive receive a message from the queue.
+func (c *Client) Receive() (msg *sqs.Message, err error) {
+	var out *sqs.ReceiveMessageOutput
+	out, err = c.client.ReceiveMessage(&sqs.ReceiveMessageInput{
+		QueueUrl:            aws.String(c.queueURL),
 		MaxNumberOfMessages: aws.Int64(1),
 		VisibilityTimeout:   aws.Int64(36000),
 		WaitTimeSeconds:     aws.Int64(0),
 	})
-
 	if err != nil {
-		fmt.Println("Something went wrong...", err)
-		return nil, err
+		err = errors.Wrap(err, "receiving sqs message failed")
+		return
 	}
 
-	if len(result.Messages) == 0 {
-		fmt.Println("Received no messages")
-		return nil, nil
-	}
-
-	for _, item := range result.Messages {
-		return item, nil
-	}
-
-	return nil, nil
+	msg = out.Messages[0]
+	return
 }
 
-// Delete SQS message with its Handle
-func Delete(sqsSvc SQSType, ReceiptHandle *string) bool {
-
-	resultDelete, err := sqsSvc.Service.DeleteMessage(&sqs.DeleteMessageInput{
-		QueueUrl:      aws.String(sqsSvc.QueueURL),
-		ReceiptHandle: ReceiptHandle,
+// Delete deletes a message from the queue.
+func (c *Client) Delete(msg *sqs.Message) error {
+	_, err := c.client.DeleteMessage(&sqs.DeleteMessageInput{
+		QueueUrl:      aws.String(c.queueURL),
+		ReceiptHandle: msg.ReceiptHandle,
 	})
-
 	if err != nil {
-		fmt.Println("Delete Error", err)
-		fmt.Println("Delete Output", resultDelete)
-		return false
+		return errors.Wrap(err, "deleting sqs message failed")
 	}
-
-	return true
+	return nil
 }
